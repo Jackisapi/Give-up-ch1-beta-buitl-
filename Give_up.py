7e25895180164147465ee65efcc023e6fb6cf27a
Jackisapi <75868911+Jackisapi@users.noreply.github.com>
2021-02-28 13:33:27 -0700

    Add files via upload

---
+++ Give_up.py
@@ -0,0 +1,72 @@
+# Write your code here :-)
+import time
+print("Welcome To Give Up")
+print("To begin type begin")
+print("to quit type give up")
+print()
+answerstart=input("to begin type begin")
+#start game by typing begin
+if (answerstart == "begin"):
+
+        print("you made the wrong choice")
+if (answerstart == "give up"):
+        print:("you made the right choice")
+
+time.sleep(1.5)
+name=input("type in your name(use your real name for emersion)")
+print(name)
+print("we may now begin")
+time.sleep(1.5)
+print("Chapter one: Awaken or give up")
+print(name)
+print("Wake up....")
+print("you open your eyes to reveal a hall way that seems to have no end")
+
+print("A: give up")
+print("B: continue walking")
+continue_walkinga=input("should you continue walking")
+
+if (continue_walkinga == "continue walking"):
+        print("You walk for hours their seems to be no end")
+if (continue_walkinga == "give up"):
+    print("you made the right choice")
+time.sleep(1.5)
+
+print("A: give up")
+print("B: continue walking")
+continue_walkingb=input("should you continue walking")
+if (continue_walkingb == "continue walking"):
+    print(" You walk for days on end and you start to starve to death do you continue walking")
+if (continue_walkingb == "give up"):
+    print("you made the right choice")
+continue_walkingc=input("Dont continue walking you will die")
+if (continue_walkingc == "continue walking"):
+        print("You continue walking there seems to be an end")
+        time.sleep(.5)
+        print("I would still advise you stop walking.")
+if (continue_walkingc == "give up"):
+    print("you made the right choice")
+print("wont you give up already your dieing continueing will result in suffering")
+continuewalkingd=input("GIVE UP")
+if (continuewalkingd == "continue walking"):
+    print(" You make it to two doors the one on the left has sign that says wake up")
+    time.sleep(1.5)
+    print("The door on the right says give up")
+if  (continuewalkingd == "give up"):
+    print("you made the right choice")
+print("Then once you start standing still you hear a whisper in your ear")
+print("wake up...")
+print("anyway what door will y")
+print("wake up..")
+time.sleep(1)
+print("Wake Up")
+time.sleep(1)
+print("Wake UP")
+whichdoor=input("which door do you pick")
+if (whichdoor == "left"):
+     print("Your eyes open to see two things")
+     print("A heart monitor")
+     print("and a Man muttering 'give up'")
+     print("End of chapter")
+if (whichdoor == "right"):
+     print("You made the right choice")
